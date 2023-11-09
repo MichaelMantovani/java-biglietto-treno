00b53e5af41d6235f9114c6b8b49f10fcf521286
MichaelMantovani <michmantovani@gmail.com>
2023-11-09 17:13:09 +0100

    finish ticket train snack

--- src/org/lessons/java/CalcolaBiglietto.java
+++ src/org/lessons/java/CalcolaBiglietto.java
@@ -1,5 +1,33 @@
 package org.lessons.java;
 
-public class CalcolaBiglietto {
+import java.util.Scanner;
 
+public class CalcolaBiglietto {
+public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	
+	System.out.println("Quanti km vuoi percorrere?");
+	float km = in.nextFloat();
+;	System.out.println("Quanti anni hai?");
+	int age = in.nextInt();
+	
+	float priceKm = 0.21F;
+	float minorsDiscount = 0.2F;
+	float elderlyDiscount = 0.4F;
+	
+	
+	float price = km * priceKm;
+	
+	float discountedPrice = 0F;
+	
+	if (age < 18) {
+		discountedPrice = price - (price * minorsDiscount);
+	} else if (age > 65) {
+		discountedPrice = price - (price * elderlyDiscount);
+	} else discountedPrice = price;
+	
+	String formatDiscountedPrice = String.format("%.2f", discountedPrice);
+	
+	System.out.println(formatDiscountedPrice + "€");
+}
 }
